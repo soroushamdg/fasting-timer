db14ec0ebb87162a333dfd8203cd87f88f25c357
soroushamdg <soroushmpour@gmail.com>
2023-01-05 18:25:55 -0500

    Initial Commit

---
+++ Fastimer/FastimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastimerApp.swift
+//  Fastimer
+//
+//  Created by Soro on 2023-01-05.
+//
+
+import SwiftUI
+
+@main
+struct FastimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
